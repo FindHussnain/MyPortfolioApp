0d0703f953e678f5bb1baa65e649e204221fd015
Hussnain Younas <findhussnain@gmail.com>
2022-02-25 17:33:22 +0500

    Implemented destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,15 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    # Destroy/delete the record
+    @portfolio_item.destroy
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Record was removed." }
+    end
+  end
 end
