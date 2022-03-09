9a6f9cb0079280a3d8211e3102e41bfb411c4d21
Hussnain Younas <findhussnain@gmail.com>
2022-03-10 02:07:08 +0500

    Implemented toggle_status feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-
   resources :blogs
-
-  root 'pages#home'
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 end
